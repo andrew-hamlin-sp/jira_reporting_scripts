e29a3ce5fdf8fa47a6fa4247eb1fff4995c1f082
Andrew Hamlin <andrew.hamlin@sailpoint.com>
2018-02-12 12:54:10 -0600

    version 0.99.16 add user-friendly headers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='qjira',
-      version='0.99.15',
+      version='0.99.16',
       description='Query JIRA Cloud REST API',
       author='Andrew Hamlin',
       author_email='andrew.hamlin@sailpoint.com',
